7b72a3a94995bba31d05ca078f9c62efc0c9813d
Mithras <dimabrook79@gmail.com>
2024-01-21 12:12:44 +0200

    add: getPrivateKeySigner & inline docs

--- src/utils/viem.ts
+++ src/utils/viem.ts
@@ -1,13 +1,14 @@
 
 import { TChainName } from '../types';
 import { findChain, getChainData } from './chains';
-import { 
-    createPublicClient, 
-    createWalletClient, 
-    custom, 
-    http, 
-    publicActions  
+import {
+    createPublicClient,
+    createWalletClient,
+    custom,
+    http,
+    publicActions
 } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts'
 
 const options = {
     retryCount: 100,
@@ -15,32 +16,61 @@ const options = {
     timeout: 100_000
 };
 
-export default function getBrowserExtentionSigner(chainName: TChainName){
+/**
+ * Creates a browser wallet signer (UI only)
+ * @param chainName the lowercased name of the targetted chain
+ * @returns a viem WalletClient with read & write access
+ */
+export default function getBrowserExtentionSigner(chainName: TChainName) {
 
     const chain = findChain(chainName);
 
-    const signer = createWalletClient({
+    return createWalletClient({
         chain,
         transport: custom((window as any).ethereum)
     })
-    .extend(publicActions); // Can act as a read only provider
-
-    return signer;
+        .extend(publicActions); // Can act as a read only provider
 
 }
 
+/**
+ * Creates a private key signer (Backend only)
+ * @param chainName the lowercased name of the targetted chain
+ * @param SK the account's private key
+ * @returns a viem WalletClient with read & write access
+ */
+export function getPrivateKeySigner(chainName: TChainName, SK: string) {
+
+    const chain = findChain(chainName);
+
+    const account = privateKeyToAccount(`0x${SK.replace('0x', '')}`);
+
+    const rpcURL: string = getChainData(chainName, "url") as string;
+
+    return createWalletClient({
+        account,
+        chain,
+        transport: http(rpcURL, (options))
+    })
+        .extend(publicActions); // Can act as a read only provider
+
+}
+
+/**
+ * Creates a chain reader
+ * @param chainName the lowercased name of the targetted chain
+ * @returns a viem Public Client with read only access
+ */
 export function getProvider(chainName: TChainName) {
 
     const chain = findChain(chainName);
 
     const rpcURL: string = getChainData(chainName, "url") as string;
 
-    const provider = createPublicClient({
+    return createPublicClient({
         chain,
         transport: http(rpcURL, (options))
     });
-
-    return provider;
 
 }
 
