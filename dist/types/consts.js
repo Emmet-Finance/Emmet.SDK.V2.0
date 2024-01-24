99dee5591d17d552a5ae213b30da307658b3f0b1
Mithras <dimabrook79@gmail.com>
2024-01-24 10:28:01 +0200

    npm publish only TypeScript

--- dist/types/consts.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.emailBackend = exports.txBackend = void 0;
-exports.txBackend = "https://testnet-tx.emmet.finance/";
-exports.emailBackend = "https://emails-backend-cc23168feba6.herokuapp.com";
